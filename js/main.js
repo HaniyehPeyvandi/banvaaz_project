8795db5f79645cfc6e63ccc98022d0a16a25620d
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-09-06 16:16:38 +0430

    add js code for change the color of navbar by scroll

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ const nav = document.querySelector(".nav");
 navToggler.addEventListener("click", (e) => {
   nav.classList.toggle("nav__expanded");
 });
+
+// Navbar
+var mainHeader = document.querySelector(".main-header");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    mainHeader.classList.add("scroll");
+  } else {
+    mainHeader.classList.remove("scroll");
+  }
+};
